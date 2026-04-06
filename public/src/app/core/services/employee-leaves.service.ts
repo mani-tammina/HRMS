ffd44b9ae98d72df0787f058a221ee0d1d70e519
ganesh-tammina <ganesh.meesala@tammina.com>
2026-04-06 12:40:41 +0530

    made cUI changes in attendance logs

--- public/src/app/core/services/employee-leaves.service.ts
+++ public/src/app/core/services/employee-leaves.service.ts
@@ -8,7 +8,7 @@ export class EmployeeLeavesService {
   private env = environment;
   private readonly API_URL = `http://${this.env.apiURL}/api/leaves`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getLeaveBalance(year: number, employeeId?: number): Observable<any> {
     const token = localStorage.getItem('token');
@@ -23,14 +23,14 @@ export class EmployeeLeavesService {
     return this.http.get<any>(`${this.API_URL}/balance`, { headers, params });
   }
 
-  initializeBalance(employeeId: number, leavePlanId: number, year: number): Observable<any> {
+  initializeBalance(year: number): Observable<any> {
     const token = localStorage.getItem('access_token') || localStorage.getItem('token');
     const headers = new HttpHeaders({
       accept: 'application/json',
       Authorization: `Bearer ${token}`,
       'Content-Type': 'application/json'
     });
-    const body = { leave_plan_id: leavePlanId, leave_year: year };
-    return this.http.post<any>(`${this.API_URL}/initialize-balance/${employeeId}`, body, { headers });
+    const body = { leave_year: year };
+    return this.http.post<any>(`${this.API_URL}/initialize-my-balance`, body, { headers });
   }
 }
